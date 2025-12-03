af84d0cce213ed09c80c74be8e4ce80d809eb76b
Richard Cho <abelsteiger@gmail.com>
2025-12-03 14:05:35 +0900

    add: project environment files

---
+++ src/test/java/com/example/lxp/ProjectLxp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLxp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
