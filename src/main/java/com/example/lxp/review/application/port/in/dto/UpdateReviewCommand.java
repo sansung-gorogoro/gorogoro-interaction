ac18a37bff32d14725289d783abef3ab0d8ac481
Richard Cho <abelsteiger@gmail.com>
2025-12-10 15:17:44 +0900

    refactor: review domain now use User VO to handle user id and role

--- src/main/java/com/example/lxp/review/application/port/in/dto/UpdateReviewCommand.java
+++ src/main/java/com/example/lxp/review/application/port/in/dto/UpdateReviewCommand.java
@@ -1,13 +1,14 @@
 package com.example.lxp.review.application.port.in.dto;
 
+import com.example.lxp.common.model.dto.User;
 import com.example.lxp.review.domain.model.Rating;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateReviewCommand(
-        @NotNull Long authorId,
         @NotNull Long courseId,
         @NotNull Long reviewId,
+        @NotNull User user,
         @NotBlank String title,
         @NotBlank String comment,
         @NotNull Rating rating
