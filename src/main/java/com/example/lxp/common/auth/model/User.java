fcaadb9d370550ab3cb60158760143370fff46fe
Richard Cho <abelsteiger@gmail.com>
2026-01-05 15:04:32 +0900

    feat: now all web controller uses VO for user information

--- src/main/java/com/example/lxp/common/auth/model/User.java
+++ src/main/java/com/example/lxp/common/auth/model/User.java
@@ -1,7 +1,31 @@
 package com.example.lxp.common.auth.model;
 
-public record User(
-        Long id,
-        Role role
-) {
+import com.example.lxp.exception.BusinessException;
+import com.example.lxp.exception.CommonErrorCode;
+
+public class User {
+
+    private Long id;
+    private Role role;
+
+    private User(Long id, Role role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public static User from(Long userIdHeader, String userRoleHeader) {
+        if (userIdHeader == null || userIdHeader <= 0 || userRoleHeader == null) {
+            throw BusinessException.builder(CommonErrorCode.INVALID_AUTH_CONTEXT).build();
+        }
+        return new User(userIdHeader, Role.from(userRoleHeader));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    
 }
